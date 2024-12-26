5dbee20ec054e12a4ba78e6047c29aa91b9326a5
nyum76 <nyum2024@gmail.com>
2024-12-26 20:49:12 +0900

    📝 시작 화면 로그인 페이지로 설정

--- spartamarket/spartamarket/urls.py
+++ spartamarket/spartamarket/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from accounts import views
 
 urlpatterns = [
+    path('',views.login, name="login"),
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
     path('accounts/', include('accounts.urls')),
