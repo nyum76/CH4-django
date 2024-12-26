1908a4dcba501f68cc1bbe4a465b8bdddd2025b3
nyum76 <nyum2024@gmail.com>
2024-12-26 17:39:16 +0900

    ✨ logout 기능 구현

--- spartamarket/accounts/views.py
+++ spartamarket/accounts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import SignupForm, ProfileForm
 from django.urls import path, include
-from django.contrib.auth import login, logout
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import login as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from .models import User
 # from products.models import Product
@@ -24,12 +26,13 @@ def signup(request):
     }
     return render(request, 'accounts/signup.html', context)
 
+@require_http_methods(["GET","POST"])
 def login(request):
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
-            login(request, user)
+            auth_login(request, user)
             return redirect("products:product_list")
     else:
         form = AuthenticationForm()
@@ -41,5 +44,5 @@ def login(request):
 
 @login_required
 def logout(request):
-    logout(request)
+    auth_logout(request)
     return redirect('accounts:login')
