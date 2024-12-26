9a0154fa15db02f5e431ba14f84654563bd7ab5e
nyum76 <nyum2024@gmail.com>
2024-12-26 19:09:58 +0900

    ✨ 상품 상세보기 기능 구현

--- spartamarket/products/urls.py
+++ spartamarket/products/urls.py
@@ -5,5 +5,7 @@ app_name = "products"
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
+    path('create/', views.product_create, name='product_create'),
+    path('<int:pk>/', views.product_detail, name='product_detail'),
 
 ]
--- spartamarket/products/views.py
+++ spartamarket/products/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render, redirect
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .forms import ProductForm
+from .models import Product
 
 def product_list(request):
-    render(request, 'products/product_list.html')
+    products = Product.objects.all()
+    return render(request, 'products/product_list.html', {'products':products})
+    
+
+@login_required
+def product_create(request):
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES, user=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('products:product_list')
+    else:
+        form = ProductForm()
+    return render(request, 'products/product_create.html', {'form':form})
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk) # 상품을 가져옴. 없으면 404 에러 발생
+    product.views += 1 # 상품 조회 수를 증가시키고
+    product.save() # 증가한 조회수 값 저장
+    return render(request, 'products/product_detail.html', {"product":product})
+    
